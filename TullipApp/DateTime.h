783373dbef673f61e59683c9216c047427af7228
Timotej Kotlín <timotej.kotlin@gmail.com>
2025-02-26 02:30:39 +0100

    Few touchups

--- TullipApp/DateTime.h
+++ TullipApp/DateTime.h
@@ -38,6 +38,8 @@ protected:
 	// \_____________________/\____/\____/
 	//           Year          Month  Day
 public:
+	Date(unsigned short day, unsigned short month, unsigned int year);
+	Date(const Date& date);
 	void SetDay(unsigned short day);
 	unsigned short GetDay();
 	void SetMonth(Month month);
@@ -45,7 +47,8 @@ public:
 	void SetYear(unsigned int year);
 	unsigned int GetYear();
 
-	void Print(const std::ostream& stream);
+	void PrintLong(const std::ostream& stream);
+	void PrintShort(const std::ostream& stream);
 	inline std::ostream& operator<<(std::ostream& stream);
 
 	static WeekDays DateToWeekDay(const Date& date);
@@ -59,6 +62,8 @@ protected:
 //          \______/ \______/ \______/
 //			  Hour     Mins     Secs
 public:
+	Time(unsigned short hours, unsigned short mins, unsigned short secs);
+	Time(const Time& time);
 	void SetSeconds(unsigned short secs);
 	unsigned short GetSeconds();
 	void SetMinutes(unsigned short mins);
@@ -66,7 +71,8 @@ public:
 	void SetHours(unsigned short years);
 	unsigned short GetHours();
 
-	void Print(const std::ostream& stream);
+	void PrintLong(const std::ostream& stream);
+	void PrintShort(const std::ostream& stream);
 	inline std::ostream& operator<<(std::ostream& stream);
 
 
@@ -75,7 +81,12 @@ public:
 
 class DateTime : public Date, public Time
 {
-	void Print(const std::ostream& stream);
+public:
+	DateTime(unsigned short day, unsigned short month, unsigned int year, unsigned short hours, unsigned short mins, unsigned short secs);
+	DateTime(const DateTime& date_time);
+	DateTime(const Date& date, const Time& time);
+	void PrintLong(const std::ostream& stream);
+	void PrintShort(const std::ostream& stream);
 	inline std::ostream& operator<<(std::ostream& stream);
 };
 
