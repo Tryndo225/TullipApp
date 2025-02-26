dce56dca88edded6b25ce2e4035a5f0a57f65461
Timotej Kotlín <timotej.kotlin@gmail.com>
2025-02-26 13:44:23 +0100

    Changes to datetime additon of timezones

--- TullipApp/DateTime.h
+++ TullipApp/DateTime.h
@@ -1,6 +1,7 @@
 #ifndef DATETIME_H_
 #define DATETIME_H_ 
 #include <iostream>
+#include <string>
 
 
 enum class Month
@@ -30,6 +31,50 @@ enum class WeekDays
 	Sunday
 };
 
+//enum class TimeZones
+//{
+//	UTC_Minus12,
+//	UTC_Minus11,
+//	UTC_Minus10,
+//	UTC_Minus9_30,
+//	UTC_Minus9,
+//	UTC_Minus8,
+//	UTC_Minus7,
+//	UTC_Minus6,
+//	UTC_Minus5,
+//	UTC_Minus4_30,
+//	UTC_Minus4,
+//	UTC_Minus3_30,
+//	UTC_Minus3,
+//	UTC_Minus2_30,
+//	UTC_Minus2,
+//	UTC_Minus1,
+//	UTC_0,
+//	UTC_Plus1,
+//	UTC_Plus2,
+//	UTC_Plus3,
+//	UTC_Plus3_30,
+//	UTC_Plus4,
+//	UTC_Plus4_30,
+//	UTC_Plus5,
+//	UTC_Plus5_30,
+//	UTC_Plus5_45,
+//	UTC_Plus6,
+//	UTC_Plus6_30,
+//	UTC_Plus7,
+//	UTC_Plus8,
+//	UTC_Plus9,
+//	UTC_Plus9_30,
+//	UTC_Plus10,
+//	UTC_Plus10_30,
+//	UTC_Plus11,
+//	UTC_Plus11_30,
+//	UTC_Plus12,
+//	UTC_Plus12_45,
+//	UTC_Plus13,
+//	UTC_Plus14
+//};
+
 class Date
 {
 protected:
@@ -46,6 +91,7 @@ public:
 	Month GetMonth();
 	void SetYear(unsigned int year);
 	unsigned int GetYear();
+	std::string GetDate();
 
 	void PrintLong(const std::ostream& stream);
 	void PrintShort(const std::ostream& stream);
@@ -59,8 +105,9 @@ class Time
 protected:
 	unsigned int time_;
 // 00000000 00000000 00000000 00000000
-//          \______/ \______/ \______/
-//			  Hour     Mins     Secs
+// \______/ \______/ \______/ \______/
+//	 Zone	  Hour     Mins     Secs
+// Zone Bit 1 = +/-, Rest is amount of 15 mins intervals
 public:
 	Time(unsigned short hours, unsigned short mins, unsigned short secs);
 	Time(const Time& time);
@@ -70,6 +117,10 @@ public:
 	unsigned short GetMinutes();
 	void SetHours(unsigned short years);
 	unsigned short GetHours();
+	void SetZone();
+	std::string GetZone();
+
+	std::string GetTime();
 
 	void PrintLong(const std::ostream& stream);
 	void PrintShort(const std::ostream& stream);
@@ -85,6 +136,8 @@ public:
 	DateTime(unsigned short day, unsigned short month, unsigned int year, unsigned short hours, unsigned short mins, unsigned short secs);
 	DateTime(const DateTime& date_time);
 	DateTime(const Date& date, const Time& time);
+	std::string GetDateTime();
+	
 	void PrintLong(const std::ostream& stream);
 	void PrintShort(const std::ostream& stream);
 	inline std::ostream& operator<<(std::ostream& stream);
